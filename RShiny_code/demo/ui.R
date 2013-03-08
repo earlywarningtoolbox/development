df4da4be95b112c7fda8cd6ad8eabca0ebc1a5c4
antagomir <antagomir@opencomp>
2013-03-08 12:30:23 +0100

    test

--- RShiny_code/demo/ui.R
+++ RShiny_code/demo/ui.R
@@ -9,7 +9,7 @@ sidebarPanel(
   sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
   	       value=min(1000, nrow(dataset)), step=500, round=0),
 
-  selectInput('x', 'X', names(dataset)),
+  #selectInput('x', 'X', names(dataset)),
   selectInput('y', 'Y', names(dataset), names(dataset)[[2]])
   #selectInput('color', 'Color', c('None', names(dataset))),
 
