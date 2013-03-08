600fe7bfd3e864508fb730b4b99677146f7c459c
antagomir <antagomir@opencomp>
2013-03-08 12:27:57 +0100

    test

--- RShiny_code/demo/server.R
+++ RShiny_code/demo/server.R
@@ -8,20 +8,21 @@ dataset <- reactive(function() {
 })
  
 output$plot <- reactivePlot(function() {
-  p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+  #p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+  p <- ggplot(foldbif, aes(x = y)) + geom_histogram()
 
-  if (input$color != 'None')
-    p <- p + aes_string(color=input$color)
+  #if (input$color != 'None')
+  #  p <- p + aes_string(color=input$color)
   
   #facets <- paste(input$facet_row, '~', input$facet_col)
   #if (facets != '. ~ .')
   #  p <- p + facet_grid(facets)
 
-  if (input$jitter)
-    p <- p + geom_jitter()
+  #if (input$jitter)
+  #  p <- p + geom_jitter()
 
-  if (input$smooth)
-    p <- p + geom_smooth()
+  #if (input$smooth)
+  #  p <- p + geom_smooth()
 
   print(p)
 
--- RShiny_code/demo/ui.R
+++ RShiny_code/demo/ui.R
@@ -9,12 +9,12 @@ sidebarPanel(
   sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
   	       value=min(1000, nrow(dataset)), step=500, round=0),
 
-  selectInput('x', 'X', names(dataset)),
+  #selectInput('x', 'X', names(dataset)),
   selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-  selectInput('color', 'Color', c('None', names(dataset))),
+  #selectInput('color', 'Color', c('None', names(dataset))),
 
-  checkboxInput('jitter', 'Jitter'),
-  checkboxInput('smooth', 'Smooth')
+  #checkboxInput('jitter', 'Jitter'),
+  #checkboxInput('smooth', 'Smooth')
 
   #selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
   #selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
