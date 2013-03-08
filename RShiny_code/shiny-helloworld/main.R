c6e8c200b7268fb7757743809b1a82c9290feab9
antagomir <antagomir@opencomp>
2013-03-08 11:12:27 +0100

    shiny helloworld

---
+++ RShiny_code/shiny-helloworld/main.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+shiny::runGitHub(repo = "development", 
+		  username = 'earlywarnings',
+         	       ref = "master", 
+	 	    subdir = "RShiny_code/shiny-helloworld")
+
+
+# from gist
+#shiny::runGist("5063316")
+## See also
+## http://rstudio.github.com/shiny/tutorial/#deployment-local
+## in package pkg/inst/appdir/
+#shiny::runApp(system.file('appdir', package='packagename'))
+## in zip url or local
+#runUrl('https://github.com/rstudio/shiny_example/archive/master.zip')
+## in Github project
+#shiny::runGitHub('shiny_example', 'rstudio')
