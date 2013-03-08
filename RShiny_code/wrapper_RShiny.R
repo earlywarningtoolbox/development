2260ee32f9e833d40d6ed172819fab077ff9d296
antagomir <antagomir@opencomp>
2013-03-08 10:41:11 +0100

    removed incrwinsize and incrbandwidth from qda_ews call in wrapper_RShiny as unused arguments

--- RShiny_code/wrapper_RShiny.R
+++ RShiny_code/wrapper_RShiny.R
@@ -17,8 +17,6 @@ timeseries <- foldbif$y
 # winsize is the size of the rolling window expressed as percentage of the timeseries length (must be numeric between 0 and 100). Default is 50\%.
 # detrending the timeseries can be detrended/filtered prior to analysis. There are four options: gaussian, linear and first-differencing.
 # bandwidth is the bandwidth used for the Gaussian kernel when gaussian filtering is applied. It is expressed as percentage of the timeseries length (must be numeric between 0 and 100).
-# incrwinsize increments the rolling window size (must be numeric between 0 and 100). 
-# incrbandwidth is the size to increment the bandwidth used for the Gaussian kernel when gaussian filtering is applied. It is expressed as percentage of the timeseries length (must be numeric between 0 and 100).
 # cutoff to estimate minima and maxima in the potential
 # logtransform logical. If TRUE data are logtransformed prior to analysis as log(X+1). Default is FALSE.
 # interpolate logical. If TRUE linear interpolation is applied to produce a timeseries of equal length as the original. Default is FALSE (assumes there are no gaps in the timeseries).
