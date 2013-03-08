c742393bb66b01d5e4b45cda539dbd2ce2582b67
antagomir <antagomir@opencomp>
2013-03-08 12:34:19 +0100

    test

--- RShiny_code/demo/ui.R
+++ RShiny_code/demo/ui.R
@@ -7,10 +7,10 @@ headerPanel("Diamonds Explorer"),
 sidebarPanel(
  
   sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(mydata),
-  	       value=min(1000, nrow(mydata)), step=500, round=0),
+  	       value=min(1000, nrow(mydata)), step=500, round=0)
 
   #selectInput('x', 'X', names(dataset)),
-  selectInput('y', 'Y', names(mydata), names(mydata)[[2]])
+  #selectInput('y', 'Y', names(mydata), names(mydata)[[2]])
   #selectInput('color', 'Color', c('None', names(dataset))),
 
   #checkboxInput('jitter', 'Jitter'),
