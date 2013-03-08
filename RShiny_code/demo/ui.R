08df7729471ae58ad22148affe3e317bd839e193
antagomir <antagomir@opencomp>
2013-03-08 16:37:03 +0100

    sidepanel version works ok

--- RShiny_code/demo/ui.R
+++ RShiny_code/demo/ui.R
@@ -1,29 +1,34 @@
 library(shiny)
 library(ggplot2)
  
-shinyUI(bootstrapPage(
- 
-  selectInput(inputId = 'analysis', 
+shinyUI(pageWithSidebar(
+
+  headerPanel("Early warnings"),
+    sidebarPanel(
+      selectInput(inputId = 'analysis', 
     		label = 'Analysis', 
 		choices = c("generic", "potential"), 
 		selected = "generic"),
 
-  # Display this only with analysis = "generic"
-  conditionalPanel(condition = "input.analysis == 'generic'",
+      # Display this only with analysis = "generic"
+      conditionalPanel(condition = "input.analysis == 'generic'",
 
-    sliderInput(inputId = "winsize",
+        sliderInput(inputId = "winsize",
         	label = "Sliding window size (percentage of the time series):",
         	min = 5, max = 100, value = 50, step = 0.2),
 
-    selectInput(inputId = 'detrending', 
+        selectInput(inputId = 'detrending', 
     		label = 'Detrending', 
 		choices = c("no", "gaussian", "linear", "first-diff"), 
 		selected = "no"),
 
-    checkboxInput('logtransform', 'Logarithmize'),
-    checkboxInput('interpolate', 'Interpolate')
+        checkboxInput('logtransform', 'Logarithmize'),
+        checkboxInput('interpolate', 'Interpolate')
+      )
+    ),
 
-  ),
-
-  plotOutput(outputId = "plot", height = "300px")
-))
+    mainPanel(
+      plotOutput(outputId = "plot", height = "300px")
+    )
+  )
+)
